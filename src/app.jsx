612ef4891085e43b32c07ca4648fbd80bc8122e3
Tim Co <timco92@gmail.com>
2021-12-23 09:04:53 -0800

    TC - initial setup

---
+++ src/app.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDom.render(<p>Expensify!</p>, document.getElementById('app'));
+
+class OldSyntax {
+  constructor() {
+    this.name = 'Tim';
+
+    // needs to bind in constructor
+    this.getGreeting = this.getGreeting.bind(this);
+  }
+
+  getGreeting() {
+    return `Hi, My name is ${this.name}.`;
+  }
+}
+
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
+
+class NewSyntax {
+  name = 'Yay';
+
+  getGreeting = () => {
+    return `Hi, my name is ${this.name}.`;
+  };
+}
+const newSyntax = new NewSyntax();
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting());
